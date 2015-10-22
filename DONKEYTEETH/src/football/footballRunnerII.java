fa961fc1cea975898bc3efb8d71680aa8769498d
753875 <753875@10.9.34.40>
2015-10-22 16:36:33 -0500

    awzsfgdfagss

--- DONKEYTEETH/src/football/footballRunnerII.java
+++ DONKEYTEETH/src/football/footballRunnerII.java
@@ -1,62 +1,67 @@
 package football;
+
 import static java.lang.System.*;
 
 import java.awt.Container;
 import java.util.Scanner;
 import javax.swing.JFrame;
+
 public class footballRunnerII {
-	
-	int intDist, intDist3, intDist4, intDown, yardsGained, intDist2Touch;
+
+	int intDist, intDist2, intDist3, intDist4, intDown, yardsGained, intDist2Touch;
 	double yds, yardsgained;
 	String run, pass, PLAY;
 	char r, p;
 	String play;
 	Scanner gage = new Scanner(System.in);
-	
-	
-	public footballRunnerII()
-	
-	{
-		
-		
-		footballSimMethods donkeyteeth = new footballSimMethods();
-		//intDown = 1;
-		intDown++;
-		intDist= 10;
-				
-		while(intDown<=4){
-		out.println("Down:"+intDown);
-		out.println("Distance:"+intDist);
-		out.println("Yards Gained:"+yardsgained);
-		PLAY = gage.next();
-		
-		
-		if (PLAY.equals("run")) {
-             yds = donkeyteeth.yds();
-        } else if (PLAY.equals("pass")) {
-        	 yds = donkeyteeth.yods();
-        } else {
-            out.print("textError");
-        }
-		
-		intDown++;
-		intDist= (int) (10-yds);
-		yardsgained= (yds);
-		if (intDist<=0) {
-			intDown = 1;
-			intDist = 10;
-			
-		}}
-		if(intDown>4){
-			out.print("TURNOVER");
-	}}
-			
-		
 
-	public static void main( String args[] )
+	public footballRunnerII()
+
 	{
+
+		footballSimMethods donkeyteeth = new footballSimMethods();
+		// intDown = 1;
+		intDown++;
+		intDist = 10;
+
+		while (intDown <= 4) {
+			out.println("Down:" + intDown);
+			out.println("Distance:" + intDist);
+			out.println("Yards Gained:" + yardsgained);
+			PLAY = gage.next();
+
+			if (PLAY.equals("run")) {
+				yds = donkeyteeth.yds();
+			} else if (PLAY.equals("pass")) {
+				yds = donkeyteeth.yods();
+			} else {
+				out.print("textError");
+			}
+			if(intDown <= 1){
+				intDist = intDist2 = (int) (10 - yds);
+			} else if (intDown<= 2) {
+				intDist = intDist3 = (int) (intDist2- yds);
+			} else if (intDown<= 3) {
+				intDist = intDist4 = (int) (intDist3- yds);
+			} else if (intDown<= 4) {
+				intDist = (int) (intDist3- yds);
+			}
+			intDown++;
+			yardsgained = (yds);
+			
+			
+			if (intDist <= 0) {
+				intDown = 1;
+				intDist = 10;
+
+			}
+		}
+		if (intDown > 4) {
+			out.print("TURNOVER");
+		}
+	}
+
+	public static void main(String args[]) {
 		footballRunnerII run = new footballRunnerII();
 	}
-	}
-
-
+}
