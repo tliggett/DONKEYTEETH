8f8bb11b243717fab10e23ba229dbadefcb6a341
753875 <753875@apps.district196.org>
2015-10-23 14:42:03 -0500

    dsfafasdf

--- DONKEYTEETH/src/football/footballRunnerII.java
+++ DONKEYTEETH/src/football/footballRunnerII.java
@@ -8,7 +8,7 @@ import javax.swing.JFrame;
 
 public class footballRunnerII {
 
-	int intDist, intDist2, intDist3, intDist4, intDown, yardsGained, intDist2Touch;
+	int yds2go, intDist, intDist2, intDist3, intDist4, intDown, yardsGained, intDist2Touch;
 	double yds, yardsgained;
 	String run, pass, PLAY;
 	char r, p;
@@ -23,21 +23,28 @@ public class footballRunnerII {
 		// intDown = 1;
 		intDown++;
 		intDist = 10;
+		yds2go = 80;
 
-		while (intDown <= 4) {
-			out.println("Down:" + intDown);
-			out.println("Distance:" + intDist);
-			out.println("Yards Gained:" + yardsgained);
+		while (intDown <= 4 && yds2go >= 0) {
+			out.println("Down: " + intDown);
+			out.println("Distance: " + intDist);
+			out.println("Yards Gained: " + yardsgained);
+			out.println("Yard Until Touchdown: " + yds2go);
 			PLAY = gage.next();
+			
+			
 
 			if (PLAY.equals("run")) {
 				yds = donkeyteeth.yds();
 			} else if (PLAY.equals("pass")) {
 				yds = donkeyteeth.yods();
 			} else {
-				out.print("textError");
+				out.println("Text Error: Please Retry");
+				PLAY = gage.next();
 			}
-			if(intDown <= 1){
+			
+			intDist= (int) (intDist-yds);
+			/*if(intDown <= 1){
 				intDist = intDist2 = (int) (10 - yds);
 			} else if (intDown<= 2) {
 				intDist = intDist3 = (int) (intDist2- yds);
@@ -45,9 +52,10 @@ public class footballRunnerII {
 				intDist = intDist4 = (int) (intDist3- yds);
 			} else if (intDown<= 4) {
 				intDist = (int) (intDist3- yds);
-			}
+			}*/
 			intDown++;
 			yardsgained = (yds);
+			yds2go = (int) (yds2go-yds);
 			
 			
 			if (intDist <= 0) {
@@ -55,6 +63,11 @@ public class footballRunnerII {
 				intDist = 10;
 
 			}
+			
+		}
+		if (yds2go <= 0) {
+			out.print("TOUCHDOWN");
+			
 		}
 		if (intDown > 4) {
 			out.print("TURNOVER");
